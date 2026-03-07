2b824a15e994cd5c6f207612ad35ed9968eee568
Sahil <paalsahil04@gmail.com>
2026-03-07 10:21:18 +0530

    update-batch

--- worker.js
+++ worker.js
@@ -43,7 +43,9 @@ async function startMatchingWorker() {
         input_job_title: currentJob.job_title || '',
         input_sector: toArray(currentJob.sector),
         input_location: toArray(currentJob.location),
-        input_experience: toArray(currentJob.experience_levels)
+        input_experience: toArray(currentJob.experience_levels),
+        input_work_mode: toArray(currentJob.work_mode), // 👈 NEW
+        input_job_type: toArray(currentJob.job_type)
       });
 
       if (rpcError) {
